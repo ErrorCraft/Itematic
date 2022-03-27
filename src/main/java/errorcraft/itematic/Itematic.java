5ee149a8c779359b41a1d0e0f001e34c375965b9
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2022-03-27 17:28:35 +0200

    Add project files

---
+++ src/main/java/errorcraft/itematic/Itematic.java
@@ -0,0 +1,9 @@
+package errorcraft.itematic;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Itematic implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
