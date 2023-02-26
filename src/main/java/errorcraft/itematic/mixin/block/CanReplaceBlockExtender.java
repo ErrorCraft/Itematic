11bc72dc61a586671faf5abea5ac4a1cbb059454
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2023-02-27 00:39:48 +0100

    Add other blocks to the canReplace mixin

--- src/main/java/errorcraft/itematic/mixin/block/CanReplaceBlockExtender.java
+++ src/main/java/errorcraft/itematic/mixin/block/CanReplaceBlockExtender.java
@@ -1,17 +1,16 @@
 package errorcraft.itematic.mixin.block;
 
 import errorcraft.itematic.item.ItemUtil;
-import net.minecraft.block.Block;
-import net.minecraft.block.SlabBlock;
+import net.minecraft.block.*;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-@Mixin(SlabBlock.class)
-public class SlabBlockExtender extends Block {
-    public SlabBlockExtender(Settings settings) {
+@Mixin({ AbstractPlantBlock.class, FlowerbedBlock.class, ScaffoldingBlock.class, SeaPickleBlock.class, SlabBlock.class, SnowBlock.class, TurtleEggBlock.class })
+public class CanReplaceBlockExtender extends Block {
+    public CanReplaceBlockExtender(Settings settings) {
         super(settings);
     }
 
