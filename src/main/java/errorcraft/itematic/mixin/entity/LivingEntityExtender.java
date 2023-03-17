7c1f47a249cd2d3491f57d3ff4a3d647d3d8770c
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2023-03-17 21:26:14 +0100

    Add effects to the food item component and add more items

--- src/main/java/errorcraft/itematic/mixin/entity/LivingEntityExtender.java
+++ src/main/java/errorcraft/itematic/mixin/entity/LivingEntityExtender.java
@@ -1,15 +1,16 @@
 package errorcraft.itematic.mixin.entity;
 
-import com.mojang.datafixers.util.Pair;
+import errorcraft.itematic.item.component.ItemComponentTypes;
+import errorcraft.itematic.item.component.components.FoodItemComponent;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.effect.StatusEffectInstance;
-import net.minecraft.item.FoodComponent;
 import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
-import java.util.List;
+import java.util.Optional;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityExtender {
@@ -24,14 +25,21 @@ public class LivingEntityExtender {
         return true;
     }
 
-    @Redirect(
-        method = "applyFoodEffects",
-        at = @At(
-            value = "INVOKE",
-            target = "Lnet/minecraft/item/FoodComponent;getStatusEffects()Ljava/util/List;"
-        )
-    )
-    private List<Pair<StatusEffectInstance, Float>> useEmptyCollection(FoodComponent instance) {
-        return List.of();
+    /**
+     * @author ErrorCraft
+     * @reason Uses the ItemComponent implementation for data-driven items.
+     */
+    @Overwrite
+    private void applyFoodEffects(ItemStack stack, World world, LivingEntity targetEntity) {
+        if (world.isClient()) {
+            return;
+        }
+        Optional<FoodItemComponent> component = stack.getComponent(ItemComponentTypes.FOOD);
+        if (component.isEmpty()) {
+            return;
+        }
+        for (FoodItemComponent.Effect effect : component.get().effects()) {
+            effect.tryApply(targetEntity, world.random);
+        }
     }
 }
