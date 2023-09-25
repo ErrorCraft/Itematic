117690cf44b16812cff11699192329e9643bfe30
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2023-09-25 13:32:53 +0200

    Sort a few keys when data generating files

--- src/client/java/net/errorcraft/itematic/data/ItematicData.java
+++ src/client/java/net/errorcraft/itematic/data/ItematicData.java
@@ -10,8 +10,15 @@ import net.errorcraft.itematic.data.server.tag.ItemGroupEntryProviderTagProvider
 import net.errorcraft.itematic.data.server.tag.ItemTagProvider;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.JsonKeySortOrderCallback;
 
 public class ItematicData implements DataGeneratorEntrypoint {
+    @Override
+    public void addJsonKeySortOrders(JsonKeySortOrderCallback callback) {
+        callback.add("id", 0);
+        callback.add("event", 0);
+    }
+
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
