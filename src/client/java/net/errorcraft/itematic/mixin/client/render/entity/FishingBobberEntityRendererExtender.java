2d60db4c259ad0634b95d918109a4abc1901f731
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2024-04-11 23:43:09 +0200

    Fix invalid mixin method reference

--- src/client/java/net/errorcraft/itematic/mixin/client/render/entity/FishingBobberEntityRendererExtender.java
+++ src/client/java/net/errorcraft/itematic/mixin/client/render/entity/FishingBobberEntityRendererExtender.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 @Mixin(FishingBobberEntityRenderer.class)
 public class FishingBobberEntityRendererExtender {
     @Redirect(
-        method = "method_59755",
+        method = "getHandPos",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"
