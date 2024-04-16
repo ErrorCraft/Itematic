992cda4d4ce7eec28929a0c6ef6f8a2549345bca
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2024-04-16 14:42:21 +0200

    Actually use the record item component fields

--- src/main/java/net/errorcraft/itematic/mixin/block/entity/JukeboxBlockEntityExtender.java
+++ src/main/java/net/errorcraft/itematic/mixin/block/entity/JukeboxBlockEntityExtender.java
@@ -1,18 +1,24 @@
 package net.errorcraft.itematic.mixin.block.entity;
 
+import net.errorcraft.itematic.item.component.ItemComponentTypes;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
 import net.minecraft.block.entity.JukeboxBlockEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.MusicDiscItem;
 import net.minecraft.registry.RegistryKeys;
 import net.minecraft.util.math.BlockPos;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.*;
 
 @Mixin(JukeboxBlockEntity.class)
-public class JukeboxBlockEntityExtender extends BlockEntity {
+public abstract class JukeboxBlockEntityExtender extends BlockEntity {
+    @Shadow
+    public abstract ItemStack getStack();
+
     public JukeboxBlockEntityExtender(BlockEntityType<?> type, BlockPos pos, BlockState state) {
         super(type, pos, state);
     }
@@ -25,7 +31,41 @@ public class JukeboxBlockEntityExtender extends BlockEntity {
         )
     )
     @SuppressWarnings("ConstantConditions")
-    private int startPlayingGetRawIdUseDynamicRegistry(Item item) {
+    private int getRawIdUseDynamicRegistry(Item item) {
         return this.world.getRegistryManager().get(RegistryKeys.ITEM).getRawId(item);
+    }
+
+    @ModifyConstant(
+        method = "tick(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)V",
+        constant = @Constant(
+            classValue = MusicDiscItem.class,
+            ordinal = 0
+        )
+    )
+    private boolean instanceOfMusicDiscItemUseItemComponentCheck(Object reference, Class<MusicDiscItem> clazz) {
+        return this.getStack().itematic$hasComponent(ItemComponentTypes.RECORD);
+    }
+
+    @ModifyVariable(
+        method = "tick(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;)V",
+        at = @At("LOAD"),
+        ordinal = 0
+    )
+    private Item castToMusicDiscItemUseNull(Item instance) {
+        return null;
+    }
+
+    @Redirect(
+        method = "isSongFinished",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/item/MusicDiscItem;getSongLengthInTicks()I"
+        )
+    )
+    private int getSongLengthInTicksUseItemComponent(MusicDiscItem instance) {
+        return this.getStack()
+            .itematic$getComponent(ItemComponentTypes.RECORD)
+            .orElseThrow()
+            .duration();
     }
 }
