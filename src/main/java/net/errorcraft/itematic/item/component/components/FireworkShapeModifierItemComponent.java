f3c0d12a805e9ea2a7da683a3b03715aa2894f22
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2024-04-16 15:49:16 +0200

    Simplify the firework_shape_modifier item component

--- src/main/java/net/errorcraft/itematic/item/component/components/FireworkShapeModifierItemComponent.java
+++ src/main/java/net/errorcraft/itematic/item/component/components/FireworkShapeModifierItemComponent.java
@@ -1,17 +1,13 @@
 package net.errorcraft.itematic.item.component.components;
 
 import com.mojang.serialization.Codec;
-import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.errorcraft.itematic.item.component.ItemComponent;
 import net.errorcraft.itematic.item.component.ItemComponentType;
 import net.errorcraft.itematic.item.component.ItemComponentTypes;
 import net.minecraft.component.type.FireworkExplosionComponent;
-import net.minecraft.util.StringIdentifiable;
 
 public record FireworkShapeModifierItemComponent(FireworkExplosionComponent.Type shape) implements ItemComponent<FireworkShapeModifierItemComponent> {
-    public static final Codec<FireworkShapeModifierItemComponent> CODEC = RecordCodecBuilder.create(instance -> instance.group(
-        StringIdentifiable.createCodec(FireworkExplosionComponent.Type::values).fieldOf("shape").forGetter(FireworkShapeModifierItemComponent::shape)
-    ).apply(instance, FireworkShapeModifierItemComponent::new));
+    public static final Codec<FireworkShapeModifierItemComponent> CODEC = FireworkExplosionComponent.Type.CODEC.xmap(FireworkShapeModifierItemComponent::new, FireworkShapeModifierItemComponent::shape);
 
     @Override
     public ItemComponentType<FireworkShapeModifierItemComponent> type() {
