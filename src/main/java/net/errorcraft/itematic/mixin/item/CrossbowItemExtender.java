f484a4acc9746471b8149d0b19ae490daad56619
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2024-05-28 20:04:18 +0200

    Fix chargeable shooter stat incrementing incorrectly

--- src/main/java/net/errorcraft/itematic/mixin/item/CrossbowItemExtender.java
+++ src/main/java/net/errorcraft/itematic/mixin/item/CrossbowItemExtender.java
@@ -1,5 +1,6 @@
 package net.errorcraft.itematic.mixin.item;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import net.errorcraft.itematic.entity.initializer.EntityInitializer;
 import net.errorcraft.itematic.item.component.ItemComponentTypes;
 import net.errorcraft.itematic.item.component.components.ProjectileItemComponent;
@@ -8,6 +9,8 @@ import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.stat.Stat;
+import net.minecraft.stat.StatType;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,6 +18,17 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(CrossbowItem.class)
 public abstract class CrossbowItemExtender {
+    @Redirect(
+        method = "shootAll",
+        at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/stat/StatType;getOrCreateStat(Ljava/lang/Object;)Lnet/minecraft/stat/Stat;"
+        )
+    )
+    private <T> Stat<Item> getOrCreateStatUseRegistryEntry(StatType<Item> instance, T key, @Local(argsOnly = true) ItemStack stack) {
+        return instance.itematic$getOrCreateStat(stack.getRegistryEntry());
+    }
+
     @Redirect(
         method = "loadProjectiles",
         at = @At(
