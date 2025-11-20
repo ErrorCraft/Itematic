0a71f36c4a1596718635dbf03fa76029f77d42e9
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2025-11-20 17:30:47 +0100

    Rename the ticks field to amount in the condition integer provider

--- src/main/java/net/errorcraft/itematic/item/use/provider/providers/ConditionIntegerProvider.java
+++ src/main/java/net/errorcraft/itematic/item/use/provider/providers/ConditionIntegerProvider.java
@@ -14,13 +14,13 @@ import net.minecraft.predicate.item.ItemPredicate;
 
 import java.util.OptionalInt;
 
-public record ConditionIntegerProvider(IntegerProvider ticks, ItemPredicate condition) implements IntegerProvider {
+public record ConditionIntegerProvider(IntegerProvider amount, ItemPredicate condition) implements IntegerProvider {
     public static final MapCodec<ConditionIntegerProvider> CODEC = RecordCodecBuilder.mapCodec(instance -> instance.group(
-        IntegerProvider.CODEC.fieldOf("ticks").forGetter(ConditionIntegerProvider::ticks),
+        IntegerProvider.CODEC.fieldOf("amount").forGetter(ConditionIntegerProvider::amount),
         ItemPredicate.CODEC.fieldOf("condition").forGetter(ConditionIntegerProvider::condition)
     ).apply(instance, ConditionIntegerProvider::new));
     public static final PacketCodec<RegistryByteBuf, ConditionIntegerProvider> PACKET_CODEC = PacketCodec.tuple(
-        IntegerProvider.PACKET_CODEC, ConditionIntegerProvider::ticks,
+        IntegerProvider.PACKET_CODEC, ConditionIntegerProvider::amount,
         ItemPredicateUtil.PACKET_CODEC, ConditionIntegerProvider::condition,
         ConditionIntegerProvider::new
     );
