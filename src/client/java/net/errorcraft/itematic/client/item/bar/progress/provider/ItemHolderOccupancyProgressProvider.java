3efc9f251b1055e5c065c2d2ddb439b533e96b18
ErrorCraft <51973682+ErrorCraft@users.noreply.github.com>
2025-12-23 21:48:00 +0100

    Fix the display for the item holder capacity

--- src/client/java/net/errorcraft/itematic/client/item/bar/progress/provider/ItemHolderOccupancyProgressProvider.java
+++ src/client/java/net/errorcraft/itematic/client/item/bar/progress/provider/ItemHolderOccupancyProgressProvider.java
@@ -2,11 +2,14 @@ package net.errorcraft.itematic.client.item.bar.progress.provider;
 
 import net.errorcraft.itematic.client.item.bar.progress.ProgressProvider;
 import net.errorcraft.itematic.client.item.bar.progress.ProgressProviderKeys;
+import net.errorcraft.itematic.component.ItematicDataComponentTypes;
 import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.BundleContentsComponent;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
 import org.apache.commons.lang3.math.Fraction;
+
+import java.util.Objects;
 
 public class ItemHolderOccupancyProgressProvider implements ProgressProvider {
     @Override
@@ -16,6 +19,9 @@ public class ItemHolderOccupancyProgressProvider implements ProgressProvider {
 
     @Override
     public boolean isVisible(ItemStack stack) {
+        if (!stack.contains(ItematicDataComponentTypes.ITEM_HOLDER_CAPACITY)) {
+            return false;
+        }
         return stack.getOrDefault(DataComponentTypes.BUNDLE_CONTENTS, BundleContentsComponent.DEFAULT)
             .getOccupancy()
             .compareTo(Fraction.ZERO) > 0;
@@ -25,6 +31,7 @@ public class ItemHolderOccupancyProgressProvider implements ProgressProvider {
     public float get(ItemStack stack) {
         return stack.getOrDefault(DataComponentTypes.BUNDLE_CONTENTS, BundleContentsComponent.DEFAULT)
             .getOccupancy()
+            .divideBy(Objects.requireNonNull(stack.get(ItematicDataComponentTypes.ITEM_HOLDER_CAPACITY)))
             .floatValue();
     }
 }
