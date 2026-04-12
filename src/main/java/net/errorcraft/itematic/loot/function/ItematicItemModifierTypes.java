8635849b39d26aa9cc33599f15317b3f6fbdb390
ErrorCraft <errorcraftderp@gmail.com>
2026-04-12 19:40:40 +0200

    Move the set_item_pointer_location action to an item modifier

--- src/main/java/net/errorcraft/itematic/loot/function/ItematicItemModifierTypes.java
+++ src/main/java/net/errorcraft/itematic/loot/function/ItematicItemModifierTypes.java
@@ -7,6 +7,7 @@ public class ItematicItemModifierTypes {
     public static final LootFunctionType<DyeItemModifier> DYE = LootFunctionTypesAccessor.register("dye", DyeItemModifier.CODEC);
     public static final LootFunctionType<SetRandomPotionItemModifier> SET_RANDOM_POTION = LootFunctionTypesAccessor.register("set_random_potion", SetRandomPotionItemModifier.CODEC);
     public static final LootFunctionType<SplitItemModifier> SPLIT = LootFunctionTypesAccessor.register("split", SplitItemModifier.CODEC);
+    public static final LootFunctionType<SetItemPointerLocationItemModifier> SET_ITEM_POINTER_LOCATION = LootFunctionTypesAccessor.register("set_item_pointer_location", SetItemPointerLocationItemModifier.CODEC);
 
     private ItematicItemModifierTypes() {}
 
--- src/main/java/net/errorcraft/itematic/loot/function/SplitItemModifier.java
+++ src/main/java/net/errorcraft/itematic/loot/function/SplitItemModifier.java
@@ -2,9 +2,11 @@ package net.errorcraft.itematic.loot.function;
 
 import com.mojang.serialization.MapCodec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.loot.condition.LootCondition;
 import net.minecraft.loot.context.LootContext;
+import net.minecraft.loot.context.LootContextParameters;
 import net.minecraft.loot.function.ConditionalLootFunction;
 import net.minecraft.loot.function.LootFunctionType;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
@@ -30,7 +32,7 @@ public class SplitItemModifier extends ConditionalLootFunction {
     }
 
     public static Builder<?> builder(int count) {
-        return builder(conditions -> new SplitItemModifier(ConstantLootNumberProvider.create(count)));
+        return builder(conditions -> new SplitItemModifier(conditions, ConstantLootNumberProvider.create(count)));
     }
 
     @Override
@@ -40,6 +42,7 @@ public class SplitItemModifier extends ConditionalLootFunction {
 
     @Override
     protected ItemStack process(ItemStack stack, LootContext context) {
-        return stack.split(this.count.nextInt(context));
+        LivingEntity holder = context.get(LootContextParameters.THIS_ENTITY) instanceof LivingEntity target ? target : null;
+        return stack.itematic$copyOrSplit(holder, this.count.nextInt(context));
     }
 }
