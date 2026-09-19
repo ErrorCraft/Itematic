5b0910feeacec0069ef602b657f7af498216f8c7
ErrorCraft <errorcraftderp@gmail.com>
2026-09-19 17:43:25 +0200

    Render the actual payment items in the Beacon screen (#128)

--- src/client/java/net/errorcraft/itematic/mixin/client/gui/screens/inventory/BeaconScreenExtender.java
+++ src/client/java/net/errorcraft/itematic/mixin/client/gui/screens/inventory/BeaconScreenExtender.java
@@ -1,10 +1,12 @@
 package net.errorcraft.itematic.mixin.client.gui.screens.inventory;
 
-import net.errorcraft.itematic.references.ItemIds;
+import com.mojang.datafixers.DataFixUtils;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.client.gui.screens.inventory.BeaconScreen;
+import net.minecraft.core.HolderSet;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceKey;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.BeaconMenu;
 import net.minecraft.world.item.Item;
@@ -14,13 +16,67 @@ import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.Slice;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BeaconScreen.class)
 public abstract class BeaconScreenExtender extends AbstractContainerScreen<BeaconMenu> {
+    @Unique
+    private static final int TICKS_PER_PAYMENT_ITEM = 30;
+    @Unique
+    private static final int PAYMENT_ITEM_COUNT = 5;
+    @Unique
+    private HolderSet<Item> paymentItems = HolderSet.empty();
+    @Unique
+    private int waitedTicks;
+    @Unique
+    private boolean goingForwards = true;
+    @Unique
+    private int currentPaymentItemIndex = 0;
+
     public BeaconScreenExtender(BeaconMenu menu, Inventory inventory, Component title) {
         super(menu, inventory, title);
+    }
+
+    @Inject(
+        method = "<init>",
+        at = @At("TAIL")
+    )
+    @SuppressWarnings("DataFlowIssue")
+    private void getPaymentItems(BeaconMenu menu, Inventory inventory, Component title, CallbackInfo info) {
+        this.paymentItems = DataFixUtils.orElseGet(
+            this.minecraft.level.registryAccess()
+                .lookupOrThrow(Registries.ITEM)
+                .get(ItemTags.BEACON_PAYMENT_ITEMS),
+            HolderSet::empty
+        );
+    }
+
+    @Inject(
+        method = "containerTick",
+        at = @At("TAIL")
+    )
+    private void increaseProgress(CallbackInfo info) {
+        if (this.paymentItems.size() <= PAYMENT_ITEM_COUNT) {
+            this.currentPaymentItemIndex = 0;
+            return;
+        }
+
+        this.waitedTicks++;
+        if (this.waitedTicks <= TICKS_PER_PAYMENT_ITEM) {
+            return;
+        }
+
+        this.waitedTicks = 0;
+        this.currentPaymentItemIndex += this.goingForwards ? 1 : -1;
+        int maxIndex = this.paymentItems.size() - PAYMENT_ITEM_COUNT;
+        if (this.currentPaymentItemIndex >= maxIndex) {
+            this.goingForwards = false;
+        } else if (this.currentPaymentItemIndex <= 0) {
+            this.goingForwards = true;
+        }
     }
 
     @Redirect(
@@ -31,8 +87,8 @@ public abstract class BeaconScreenExtender extends AbstractContainerScreen<Beaco
             ordinal = 0
         )
     )
-    private ItemStack newItemStackForNetheriteIngotUseCreateStack(ItemLike item) {
-        return this.createStack(ItemIds.NETHERITE_INGOT);
+    private ItemStack newItemStackForFirstItemUseHolder(ItemLike item) {
+        return this.createPaymentStack(0);
     }
 
     @Redirect(
@@ -50,8 +106,8 @@ public abstract class BeaconScreenExtender extends AbstractContainerScreen<Beaco
             )
         )
     )
-    private ItemStack newItemStackForEmeraldUseCreateStack(ItemLike item) {
-        return this.createStack(ItemIds.EMERALD);
+    private ItemStack newItemStackForSecondItemUseHolder(ItemLike item) {
+        return this.createPaymentStack(1);
     }
 
     @Redirect(
@@ -69,8 +125,8 @@ public abstract class BeaconScreenExtender extends AbstractContainerScreen<Beaco
             )
         )
     )
-    private ItemStack newItemStackForDiamondUseCreateStack(ItemLike item) {
-        return this.createStack(ItemIds.DIAMOND);
+    private ItemStack newItemStackForThirdItemUseHolder(ItemLike item) {
+        return this.createPaymentStack(2);
     }
 
     @Redirect(
@@ -88,8 +144,8 @@ public abstract class BeaconScreenExtender extends AbstractContainerScreen<Beaco
             )
         )
     )
-    private ItemStack newItemStackForGoldIngotUseCreateStack(ItemLike item) {
-        return this.createStack(ItemIds.GOLD_INGOT);
+    private ItemStack newItemStackForFourthItemUseHolder(ItemLike item) {
+        return this.createPaymentStack(3);
     }
 
     @Redirect(
@@ -107,16 +163,21 @@ public abstract class BeaconScreenExtender extends AbstractContainerScreen<Beaco
             )
         )
     )
-    private ItemStack newItemStackForIronIngotUseCreateStack(ItemLike item) {
-        return this.createStack(ItemIds.IRON_INGOT);
+    private ItemStack newItemStackForFifthItemUseHolder(ItemLike item) {
+        return this.createPaymentStack(4);
     }
 
     @Unique
-    private ItemStack createStack(ResourceKey<Item> item) {
-        if (this.minecraft.level == null) {
+    private ItemStack createPaymentStack(int offset) {
+        if (this.paymentItems.size() == 0) {
             return ItemStack.EMPTY;
         }
 
-        return this.minecraft.level.itematic$createStack(item);
+        int index = this.currentPaymentItemIndex + offset;
+        if (index >= this.paymentItems.size()) {
+            return ItemStack.EMPTY;
+        }
+
+        return new ItemStack(this.paymentItems.get(index));
     }
 }
